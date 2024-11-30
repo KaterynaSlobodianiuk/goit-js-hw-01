c1e791b61005e1e9285e7e75c3ffbf54d957ec03
KaterynaSlobodianiuk <kat.luchko@gmail.com>
2024-11-30 18:34:36 +0200

    #1

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,9 @@
-const quantity = prompt('How many droids do you want to order?')
-const pricePerDroid = 3000;
-const totalPrice = pricePerDroid * quantity;
 
-const makeTransaction = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-
-console.log(makeTransaction);
+function makeTransaction(quantity, pricePerDroid) {
+  return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+}
 
 
-
-// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
